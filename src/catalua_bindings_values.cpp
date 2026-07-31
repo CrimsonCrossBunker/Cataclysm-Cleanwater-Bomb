35f11886bd63a36cdde7fdef014d3af156126cd2
LYHGLYTX <139665200+LYHGLYTX@users.noreply.github.com>
2026-07-31 14:59:12 +0800

    build(lua): gate value binding translation units

--- src/catalua_bindings_values.cpp
+++ src/catalua_bindings_values.cpp
@@ -1,3 +1,5 @@
+#if CATA_ENABLE_LUA_UI
+
 #include "catalua_bindings_values.h"
 
 #include <algorithm>
@@ -1115,3 +1117,5 @@ void install_value_type_api(
 }
 
 } // namespace cata::lua_ui
+
+#endif // CATA_ENABLE_LUA_UI
