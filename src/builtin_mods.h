481244246080d82ef7ab3335105894f4f2e35360
LunaGlaze <shiqi.luna@outlook.com>
2026-08-01 16:22:39 +0800

    Add builtin mod manifest fallback header

---
+++ src/builtin_mods.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CATA_SRC_BUILTIN_MODS_H
+#define CATA_SRC_BUILTIN_MODS_H
+
+#if __has_include( "builtin_mods_generated.h" )
+    #include "builtin_mods_generated.h"
+#else
+    #include <array>
+    #include <string_view>
+
+inline constexpr bool builtin_mod_manifest_available = false;
+inline constexpr std::array<std::string_view, 0> builtin_mod_ids = {};
+#endif
+
+#endif // CATA_SRC_BUILTIN_MODS_H
