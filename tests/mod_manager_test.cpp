1be8adfecec217b5487953e62929c7fcfa6c4323
LunaGlaze <shiqi.luna@outlook.com>
2026-08-01 17:19:15 +0800

    Follow mod manager test state conventions

--- tests/mod_manager_test.cpp
+++ tests/mod_manager_test.cpp
@@ -5,27 +5,29 @@
 #include "mod_manager.h"
 #include "path_info.h"
 
+static const mod_id mod_dda( "dda" );
+static const mod_id mod_third_party( "test_third_party_mod" );
+static const mod_id mod_user( "test_user_mod" );
+static const mod_id mod_virtual( "test_third_party_mod#dda" );
+
 TEST_CASE( "unexpected_builtin_mod_detection", "[mod_manager]" )
 {
-    const bool original_test_mode = test_mode;
-    on_out_of_scope restore_test_mode( [&original_test_mode]() {
-        test_mode = original_test_mode;
-    } );
+    restore_on_out_of_scope<bool> restore_test_mode( test_mode );
 
     MOD_INFORMATION builtin_mod;
-    builtin_mod.ident = mod_id( "dda" );
+    builtin_mod.ident = mod_dda;
     builtin_mod.path = PATH_INFO::moddir() / "dda";
 
     MOD_INFORMATION third_party_mod;
-    third_party_mod.ident = mod_id( "test_third_party_mod" );
+    third_party_mod.ident = mod_third_party;
     third_party_mod.path = PATH_INFO::moddir() / "test_third_party_mod";
 
     MOD_INFORMATION user_mod;
-    user_mod.ident = mod_id( "test_user_mod" );
+    user_mod.ident = mod_user;
     user_mod.path = PATH_INFO::user_moddir_path() / "test_user_mod";
 
     MOD_INFORMATION virtual_mod;
-    virtual_mod.ident = mod_id( "test_third_party_mod#dda" );
+    virtual_mod.ident = mod_virtual;
     virtual_mod.path = PATH_INFO::moddir() / "test_third_party_mod";
 
     test_mode = false;
