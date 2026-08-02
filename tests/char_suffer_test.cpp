91e2976869a69b2d16eeea29ea8fa215bef04127
LunaGlaze <shiqi.luna@outlook.com>
2026-08-02 16:00:48 +0800

    Test torso neck crowd protection

--- tests/char_suffer_test.cpp
+++ tests/char_suffer_test.cpp
@@ -10,6 +10,7 @@
 #include "character.h"
 #include "coordinates.h"
 #include "creature.h"
+#include "damage.h"
 #include "flag.h"
 #include "game.h"
 #include "item.h"
@@ -25,6 +26,9 @@
 
 static const efftype_id effect_grabbed( "grabbed" );
 static const efftype_id effect_crowd_crushed( "crowd_crushed" );
+
+static const damage_type_id damage_bash( "bash" );
+static const sub_bodypart_str_id sub_body_part_torso_neck( "torso_neck" );
 
 static const itype_id itype_test_hazmat_suit( "test_hazmat_suit" );
 static const itype_id itype_test_longshirt( "test_longshirt" );
@@ -492,6 +496,16 @@ TEST_CASE( "suffering_from_asphyxiation", "[char][suffer][oxygen][grab]" )
                 test_suffer( dummy, 3_turns, true );
                 CHECK( dummy.oxygen == 15 );
                 CHECK_FALSE( dummy.has_effect( effect_crowd_crushed ) );
+            }
+        }
+
+        WHEN( "their torso neck is protected by armor" ) {
+            REQUIRE( dummy.wear_item( item( itype_test_hazmat_suit ), false ).has_value() );
+            damage_instance pressure( damage_bash, 1 );
+            dummy.absorb_hit( sub_body_part_torso_neck.id(), pressure );
+
+            THEN( "the pressure is absorbed without harming the character" ) {
+                CHECK( pressure.total_damage() == 0.0f );
             }
         }
 
