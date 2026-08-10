a24e9a6ab39372ac0837d1763d3f30932e73f9e7
LYHGLYTX <139665200+LYHGLYTX@users.noreply.github.com>
2026-08-10 18:32:59 +0800

    mods(lua): add main example module

---
+++ data/mods/Lua_First_Example/main.lua
@@ -0,0 +1,9 @@
+local ccb = require("ccb")
+local content = require("content.cleanwater_charm")
+local behaviour = require("runtime.behaviour")
+
+ccb.runtime.handler("use_cleanwater_charm", behaviour.use_charm, 1)
+ccb.runtime.handler("lua_first_example_ready", behaviour.world_ready, 1)
+ccb.runtime.on("world_ready", "lua_first_example_ready")
+
+content.register(ccb)
