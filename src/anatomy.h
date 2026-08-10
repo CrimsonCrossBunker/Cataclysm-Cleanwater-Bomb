b74deadcb758b384c743c0c9915b480da2f03e05
LYHGLYTX <139665200+LYHGLYTX@users.noreply.github.com>
2026-08-10 18:31:00 +0800

    feat(lua): declare anatomy creature catalog support

--- src/anatomy.h
+++ src/anatomy.h
@@ -13,12 +13,18 @@
 class Creature;
 class JsonObject;
 
+namespace cata::lua_platform
+{
+class content_transaction;
+} // namespace cata::lua_platform
+
 /**
  * A structure that contains body parts.
  * Keeps caches for weighted selections.
  */
 class anatomy
 {
+        friend class cata::lua_platform::content_transaction;
     private:
         std::vector<bodypart_str_id> unloaded_bps;
         std::vector<bodypart_id> cached_bps;
