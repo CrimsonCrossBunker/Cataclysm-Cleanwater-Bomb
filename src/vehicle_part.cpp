8423994e8e74f2f71a7fdb54af83228e5bad8d4a
LunaGlaze <shiqi.luna@outlook.com>
2026-08-13 22:44:53 +0800

    vpart: pass cargo_spoil_multiplier down to tank contents

--- src/vehicle_part.cpp
+++ src/vehicle_part.cpp
@@ -547,7 +547,7 @@ void vehicle_part::process_contents( map &here, const tripoint_bub_ms &pos, cons
     } else if( enabled && info().has_flag( VPFLAG_HEATED_TANK ) ) {
         flag = temperature_flag::HEATER;
     }
-    base.process( here, nullptr, pos, 1, flag );
+    base.process( here, nullptr, pos, 1, flag, info().cargo_spoil_multiplier / 100.0f );
 }
 
 bool vehicle_part::fill_with( item &liquid, int qty )
