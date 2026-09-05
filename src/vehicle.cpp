40f97eb95a7f4b54ed0917e13489b97608d03dfd
LYHGLYTX <139665200+LYHGLYTX@users.noreply.github.com>
2026-09-05 20:11:17 +0800

    fix(power): 修正气泡外车辆坐标与电缆端点查找

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -6105,9 +6105,7 @@ vehicle *vehicle::find_vehicle_using_parts( const map &here,  const tripoint_abs
         return &vp->vehicle();
     }
     // Nope. Load up its submap...
-    point_sm_ms_ib veh_in_sm;
-    tripoint_abs_sm veh_sm;
-    std::tie( veh_sm, veh_in_sm ) = project_remain<coords::sm>( where );
+    const tripoint_abs_sm veh_sm = project_to<coords::sm>( where );
     const submap *sm = MAPBUFFER.lookup_submap( veh_sm );
     if( sm == nullptr ) {
         return nullptr;
@@ -6116,10 +6114,7 @@ vehicle *vehicle::find_vehicle_using_parts( const map &here,  const tripoint_abs
     for( const auto &elem : sm->vehicles ) {
         vehicle *found_veh = elem.get();
         for( const vpart_reference &vp : found_veh->get_all_parts() ) {
-            point_sm_ms_ib vp_in_sm;
-            tripoint_bub_sm vp_sm;
-            std::tie( vp_sm, vp_in_sm ) = project_remain<coords::sm>( vp.pos_bub( here ) );
-            if( vp_in_sm == veh_in_sm ) {
+            if( vp.pos_abs() == where ) {
                 return found_veh;
             }
         }
