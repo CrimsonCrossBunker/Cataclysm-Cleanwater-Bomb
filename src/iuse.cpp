3a79b48c802276d1a5b7821fc3f5148ee8e3f36f
LunaGlaze <shiqi.luna@outlook.com>
2026-09-10 18:48:46 +0800

    Remove WEED_CAKE iuse function
    Co-authored-by: worm-girl <94415528+worm-girl@users.noreply.github.com>

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -789,30 +789,6 @@ std::optional<int> iuse::anticonvulsant( Character *p, item *, const tripoint_bu
     return 1;
 }
 
-std::optional<int> iuse::weed_cake( Character *p, item *, const tripoint_bub_ms & )
-{
-    p->add_msg_if_player(
-        _( "You start scarfing down the delicious cake.  It tastes a little funny, though…" ) );
-    time_duration duration = 12_minutes;
-    if( p->has_trait( trait_TOLERANCE ) ) {
-        duration = 9_minutes;
-    }
-    if( p->has_trait( trait_LIGHTWEIGHT ) ) {
-        duration = 15_minutes;
-    }
-    p->mod_hunger( 2 );
-    p->mod_thirst( 6 );
-    if( p->get_painkiller() < 5 ) {
-        p->set_painkiller( ( p->get_painkiller() + 3 ) * 2 );
-    }
-    p->add_effect( effect_weed_high, duration );
-    p->mod_moves( -to_moves<int>( 1_seconds ) );
-    if( one_in( 5 ) ) {
-        weed_msg( *p );
-    }
-    return 1;
-}
-
 std::optional<int> iuse::coke( Character *p, item *, const tripoint_bub_ms & )
 {
     p->add_msg_if_player( _( "You snort a bump of coke." ) );
