324749cfacd1e9f54c858f39dd5a47a471d2f7c1
LunaGlaze <shiqi.luna@outlook.com>
2026-09-12 00:53:57 +0800

    Skip fault rate multiplier for debug fault toggling

--- src/character.cpp
+++ src/character.cpp
@@ -4437,7 +4437,7 @@ void Character::mend_item( item_location &&obj, bool interactive )
             if( opts[menu.ret].second ) {
                 obj->remove_fault( opts[menu.ret].first );
             } else {
-                obj->set_fault( opts[menu.ret].first, true, nullptr );
+                obj->set_fault( opts[menu.ret].first, true, nullptr, true );
             }
         }
         return;
